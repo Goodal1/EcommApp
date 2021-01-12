88ad1549bc17a476e4b2e8bda71f769307c3ad00
Goodal1 <58956923+Goodal1@users.noreply.github.com>
2021-01-12 22:47:04 +0100

    Share on github

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+const data =
+{
+    products :[
+        {_id:1,
+        name: 'Nike Crampons',
+        category:'Shirts',
+        image:'./images/Product1.jpg',
+        price: 70,
+        brand:'Nike',
+        rating: 3,
+        numReviews: 9, 
+        description : 'La sdfqualité frere'        
+        },
+         {_id:2,
+        name: 'Nike Hoops',
+        category:'Shirts',
+        image:'./images/Product1.jpg',
+        price: 110,
+        brand:'Nike',
+        rating: 2,
+        numReviews: 8,
+        description : 'Laesfzs qualité frere'        
+        },
+        {_id:3,
+        name: 'Nike Crampons',
+        category:'Shoes',
+        image:'./images/Product1.jpg',
+        price: 80,
+        brand:'Nike',
+        rating: 3,
+        numReviews: 7,
+        description : 'La qufddgfalité frere'        
+        },
+        {_id:4,
+        name: 'Nike Crampons',
+        category:'Shoes',
+        image:'./images/Product1.jpg',
+        price: 150,
+        brand:'Nike',
+        rating: 5,
+        numReviews: 6,
+        description : 'La qusdgsalité frere'        
+        },
+        {_id:5,
+        name: 'Nike Jojo',
+        category:'Pants',
+        image:'./images/Product1.jpg',
+        price: 150,
+        brand:'Nike',
+        rating: 4.5,
+        numReviews: 5,
+        description : 'La quadfdssfdlité frere'        
+        },
+        {_id:6,
+        name: 'Nike Lol',
+        category:'Pants',
+        image:'./images/Product1.jpg',
+        price: 90,
+        brand:'Nike',
+        rating: 2,
+        numReviews: 4,
+        description : 'La qualité frere'        
+        }    
+]
+}
+
+export default data;
